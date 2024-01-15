8ba2d64a48e07c5bd9b72b4e3a33a49f27a269ce
Cem2204 <cem.cem.strix@gmail.com>
2024-01-15 18:33:51 +0300

    Design

--- basicCalculator/ViewController.swift
+++ basicCalculator/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secondText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func sumClicked(_ sender: Any) {
+        let firstNum = Int(firstText.text!)!
+        let secondNum = Int(secondText.text!)!
+        resultLabel.text = String(firstNum + secondNum)
+        
+    }
+    
+    @IBAction func minusClicked(_ sender: Any) {
+    }
+    @IBAction func multiplyClicked(_ sender: Any) {
+    }
+    @IBAction func divideClicked(_ sender: Any) {
+    }
 }
 
